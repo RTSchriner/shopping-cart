50e157cd39580a1b98fba5e605364d71b0699d8c
Riley Schriner <RTSchriner@gmail.com>
2015-05-04 09:46:52 -0400

    Added testing deps and test folder/file

---
+++ test/test-server.js
@@ -0,0 +1,7 @@
+describe('Shopping List', function() {
+    it('should list items on get');
+    it('should add an item on post');
+    it('should edit an item on put');
+    it('should delete an item on delete');
+    it('should should send an error when an item it deleted but the id does not exist');
+});
